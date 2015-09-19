8ab2a0b22f11c3627214a24d9a62ce823ee94b60
Abhishek S P <abhisheksp1993@gmail.com>
2015-09-19 12:07:34 +0530

    Refactored ALive Nieghbours Count

--- src/com/thoughtworks/base2/CellGrid.java
+++ src/com/thoughtworks/base2/CellGrid.java
@@ -10,13 +10,25 @@ public class CellGrid {
 
     public int aliveNeighboursCount(int xCoordinate, int yCoordinate) {
         int count = 0;
+        count += verticalNeighboursCount(xCoordinate, yCoordinate);
+        count += horizontalNeighboursCount(cells[xCoordinate], yCoordinate);
+        return count;
+    }
+
+    private int verticalNeighboursCount(int xCoordinate, int yCoordinate) {
+        int count = 0;
         if ((xCoordinate + 1 < cells.length) && cells[xCoordinate + 1][yCoordinate].isAlive())
             count++;
         if ((xCoordinate - 1 >= 0) && cells[xCoordinate - 1][yCoordinate].isAlive())
             count++;
-        if ((yCoordinate + 1 < cells[0].length) && cells[xCoordinate][yCoordinate + 1].isAlive())
+        return count;
+    }
+
+    private int horizontalNeighboursCount(Cell[] cell, int yCoordinate) {
+        int count = 0;
+        if ((yCoordinate + 1 < cells[0].length) && cell[yCoordinate + 1].isAlive())
             count++;
-        if ((yCoordinate - 1 >= 0) && cells[xCoordinate][yCoordinate - 1].isAlive())
+        if ((yCoordinate - 1 >= 0) && cell[yCoordinate - 1].isAlive())
             count++;
         return count;
     }
